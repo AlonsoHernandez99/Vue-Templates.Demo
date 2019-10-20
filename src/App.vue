9b0b13405876ef43a0f7e2da9e93502c8590c4f2
Alonso Hdezz <luisalonso123421@gmail.com>
2019-10-20 16:09:23 -0600

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,77 @@
+<template>
+  <div id="app" class="container">
+
+    <div class="row mb-5">
+      <div class="col-12">
+
+        <h4>Parent</h4>
+        <table class="table">
+          <tr class="p-2">
+            <td><p>Value with object as prop (reference) of parent:</p></td>
+            <td>{{ valueReference }}</td>
+          </tr>
+
+          <tr class="p-2">
+            <td><p>Value with emit of parent::</p></td>
+            <td>{{ valueEmit }}</td>
+          </tr>
+        </table>  
+
+      </div>
+    </div>
+
+    <div class="row">
+
+      <div class="col-6">
+        <ChildReference :valueReference="valueReference">
+        </ChildReference>
+      </div>
+
+      <div class="col-6">
+        <ChildEmit :initialValue="valueEmit"
+                  @update-value-by-child="updateParentValue">
+        </ChildEmit>
+      </div>
+
+    </div>
+
+   
+
+  </div>
+</template>
+
+<script>
+import ChildReference from "./components/ChildReference";
+import ChildEmit from "./components/ChildEmit";
+
+export default {
+  name: "App",
+  components: {
+    ChildReference,
+    ChildEmit
+  },
+  data() {
+    return {
+      valueReference: {},
+      valueEmit: {}
+    }
+  },
+  methods: {
+    updateParentValue(newValue) {
+      console.log("new value", newValue)
+      this.valueEmit = newValue;
+    }
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/ChildEmit.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="child-emit">
+    <h4>Child with emit</h4>
+    <p>Value of child:</p> 
+    {{ valueEmit }}
+    <br>
+    <br>
+    Value cat: <input type="text"
+          @input="$emit('update-value-by-child', valueEmit)"
+          v-model="valueEmit.cat">
+    <br>
+    Value dog:<input type="text"
+          
+          v-model="valueEmit.dog">    
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ChildEmit",
+  props: {
+    initialValue: Object
+  },
+  data() {
+    return {
+      valueEmit: Object.assign({}, this.initialValue)
+    };
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1,
+h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
